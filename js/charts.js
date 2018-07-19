015d813413de23d1704bc86c2e5584d3199138c9
hong <970684357@qq.com>
2018-07-19 16:27:57 +0800

    first week

---
+++ js/charts.js
@@ -0,0 +1,281 @@
+function randomData() {
+    now = new Date(+now + oneDay);
+    value = value + Math.random() * 21 - 10;
+    return {
+        name: now.toString(),
+        value: [
+            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+            Math.round(value)
+        ]
+    }
+}
+
+var data = [];
+var now = +new Date(1997, 9, 3);
+var oneDay = 24 * 3600 * 1000;
+var value = Math.random() * 1000;
+for (var i = 0; i < 1000; i++) {
+    data.push(randomData());
+}
+
+// 实时图表1
+var mainChart1 = echarts.init(document.getElementById('realTimeChart'));
+option1 = {
+    title: {
+        text: 'PH值(水源地) 实时数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+mainChart1.setOption(option1);
+window.addEventListener('resize',function(){mainChart1.resize()})
+
+// 实时图表2
+var mainChart2 = echarts.init(document.getElementById('realTimeChart2'));
+option2 = {
+    title: {
+        text: 'PH值(成品水) 实时数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+mainChart2.setOption(option1);
+window.addEventListener('resize',function(){mainChart2.resize()})
+
+// 历史图表1
+var historyChart1 = echarts.init(document.getElementById('historyChart1'));
+var historyChart2 = echarts.init(document.getElementById('historyChart2'));
+var historyChart3 = echarts.init(document.getElementById('historyChart3'));
+optionH1 = {
+    title: {
+        text: '当天数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+optionH2 = {
+    title: {
+        text: '近七天数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+optionH3 = {
+    title: {
+        text: '近一月数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+historyChart1.setOption(optionH1);
+window.addEventListener('resize',function(){historyChart1.resize()})
+historyChart2.setOption(optionH2);
+window.addEventListener('resize',function(){historyChart2.resize()})
+historyChart3.setOption(optionH3);
+window.addEventListener('resize',function(){historyChart3.resize()})
+
+// 日期选择器查询图表
+var selectChart = echarts.init(document.getElementById('selectChart'));
+optionS = {
+    title: {
+        text: '历史数据'
+    },
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+            animation: false
+        }
+    },
+    xAxis: {
+        type: 'time',
+        splitLine: {
+            show: false
+        }
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+            show: false
+        }
+    },
+    series: [{
+        name: '模拟数据',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: data
+    }]
+};
+selectChart.setOption(optionS);
+window.addEventListener('resize',function(){selectChart.resize()})
+
+setInterval(function () {
+
+    for (var i = 0; i < 5; i++) {
+        data.shift();
+        data.push(randomData());
+    }
+
+    mainChart1.setOption({
+        series: [{
+            data: data
+        }]
+    });
+
+    mainChart2.setOption({
+        series: [{
+            data: data
+        }]
+    });
+}, 1000);
--- js/main.js
+++ js/main.js
@@ -1,113 +1,226 @@
-Vue.component('data-div',{
-  template:'\
-  <el-col :xl="6" :lg="8" :sm="12">\
-    <div class="showdata" v-on:click="$emit(\'jump\',item.id)">\
+Vue.component('data-div', {
+  template: '\
+  <el-col :xl="6" :lg="8" :md="12">\
+    <div class="showdata" v-bind:class=\'{"blue-border":blue,"green-border":green,"purple-border":purple}\' v-on:click="$emit(\'jump\',item.id)">\
       <p class="itemName"> {{ item.name }} </p>\
-      <p class="itemInfo"><span class="itemType">&nbsp水源&nbsp&nbsp：</span>\
-        <span class="itemData"> {{ item.Data1 }} </span>\
+      <p class="itemInfo"><span class="itemType">{{ title1 }}：</span>\
+        <span class="itemData"> {{ item.data1 }} </span>\
         <span class="dataRange">正常范围：&nbsp{{ item.range1 }}</span>\
       </p>\
-      <p class="itemInfo"><span class="itemType">成品水：</span>\
-        <span class="itemData">{{ item.Data2 }}</span>\
+      <p class="itemInfo"><span class="itemType">{{ title2 }}：</span>\
+        <span class="itemData"> {{ item.data2 }} </span>\
         <span class="dataRange">正常范围：&nbsp{{ item.range2 }}</span>\
       </p>\
     </div>\
   </el-col>',
-  props:['item']
+  props: ['item', 'blue', 'green', 'purple', 'title1', 'title2','num'],
+  data: function() {
+    return {}
+  },
+  mounted: function() {}
 })
-var vm = new Vue ({
-  el:'#app',
-  data:{
-    items:[
-      {
-        id:1,
-        name:'PH值',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: [{
+        id: 1,
+        name: 'PH值',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:2,
-        name:'电导率',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 2,
+        name: '电导率',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:3,
-        name:'溶解氧',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 3,
+        name: '溶解氧',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:4,
-        name:'浊度',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 4,
+        name: '浊度',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:5,
-        name:'COD-Mn',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 5,
+        name: 'COD-Mn',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:6,
-        name:'BOD',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 6,
+        name: 'BOD',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:7,
-        name:'氨氮',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 7,
+        name: '氨氮',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:8,
-        name:'总氮',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 8,
+        name: '总氮',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:9,
-        name:'总磷',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 9,
+        name: '总磷',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       },
       {
-        id:10,
-        name:'TOC',
-        Data1:'99',
-        Data2:'99',
-        range1:'xx-xx',
-        range2:'xx-xx',
+        id: 10,
+        name: 'TOC',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx',
       }
-    ]
+    ],
+    items2: [{
+        id: 11,
+        name: '1号车间',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      },
+      {
+        id: 12,
+        name: '2号车间',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      },
+      {
+        id: 13,
+        name: '3号车间',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      }
+    ],
+    items3: [
+      {
+        id: 14,
+        name: '1号仓库',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      },
+      {
+        id: 15,
+        name: '2号仓库',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      },
+      {
+        id: 16,
+        name: '3号仓库',
+        data1: '99',
+        data2: '99',
+        range1: 'xx-xx',
+        range2: 'xx-xx'
+      }
+    ],
+    options: [{
+      value: '选项1',
+      label: 'PH值'
+    }, {
+      value: '选项2',
+      label: '水温'
+    }, {
+      value: '选项3',
+      label: '电导率'
+    }, {
+      value: '选项4',
+      label: '溶解氧'
+    }, {
+      value: '选项5',
+      label: '浊度'
+    }],
+    value: '',
+    pickerOptions2: {
+      shortcuts: [{
+        text: '最近一周',
+        onClick(picker) {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+          picker.$emit('pick', [start, end]);
+        }
+      }, {
+        text: '最近一个月',
+        onClick(picker) {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+          picker.$emit('pick', [start, end]);
+        }
+      }, {
+        text: '最近三个月',
+        onClick(picker) {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+          picker.$emit('pick', [start, end]);
+        }
+      }]
+    },
+    value6: '',
+    value7: ''
   },
   methods: {
-    jump: function (id) {
-      alert(id)
+    jump: function(id) {
+      location.href='charts.html'
+    },
+    toTemp:function(){
+      if(location.href.indexOf('index.html')==-1){
+        location.href='index.html'
+      }
+      var anchor = this.$el.querySelector('#temperature')
+      document.documentElement.scrollTop = anchor.offsetTop
+    },
+    toTop:function(){
+      if(location.href.indexOf('index.html')==-1){
+        location.href='index.html'
+      }
+      document.documentElement.scrollTop = 0
     }
   }
 })
 
-//Vue.set(vm.items[1],'Data1',"88")
+
 // var Ctor = Vue.extend(Main)
 // new Ctor().$mount('#app')
